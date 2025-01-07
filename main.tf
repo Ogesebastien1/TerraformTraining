9f3e028fb3615da283f818a085835dce470e785e
Ogesebastien1 <oge.sebastien67@gmail.com>
2025-01-07 09:59:36 +0100

    Add Azure Kubernetes Cluster resource and documentation

--- main.tf
+++ main.tf
@@ -6,3 +6,24 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.aks_cluster_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
